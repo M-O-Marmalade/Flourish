18cd3b3950cf3343e4d77dd12a448076107bb956
M. O. Marmalade <xephyrpanda@yahoo.com>
2020-06-27 14:00:00 -0700

    Added a destructive workflow that has little to no lag, updated Readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 A custom tool for Renoise, used to create strums, flourishes, ripples, arpeggios, etc
 
-test
+#TO_DO
+-Allow flourishes to span multiple patterns
+-Implement tension in strums (logarithmic distribution of notes)
+-Reduce/remove lag from Non-Destructive workflow
+-Improve automatic behaviour when opening/closing Flourish window, manipulating checkboxes, setting a new line to edit, etc...
+-Add ability to strum to/from the first/last note in the line interchangeably
--- main.lua
+++ main.lua
@@ -17,7 +17,9 @@ notes_detected = 0
 
 time = 0
 tension = 0
-auto_apply = false
+auto_apply = true
+non_destructive = false
+columns_to_clear = {}
 visible_columns_only = true
 
 local vb = renoise.ViewBuilder() 
@@ -30,6 +32,51 @@ window_content = nil
 local function show_status(message)
   app:show_status(message)
   print(message)
+end
+
+--CLEAR COLUMNS_TO_CLEAR-----------------------------------------------------------------------------
+local function clear_columns_to_clear()
+  for key,value in pairs(columns_to_clear) do
+    print("\nkey: " .. key .. "  previous value: " .. value)
+    columns_to_clear[key] = 0
+    print("  new value: " .. columns_to_clear[key])
+  end
+end
+
+--STORE SONG LINES-----------------------------------------------------------------------------------
+local function store_song_lines()
+
+--...we store the line values of all lines in track in song
+      local y = 1
+      for pos,line in song.pattern_iterator:lines_in_track(track_index,true) do
+    
+        lns_in_sng[y] = {}
+    
+        local z = 1
+        while z < 13 do
+      
+          print("\nz = " .. z)
+      
+          lns_in_sng[y][z] = {
+          song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).note_value,
+          song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).instrument_value,
+          song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).volume_value,
+          song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).panning_value,
+          song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).delay_value,
+          song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).effect_number_value,
+          song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).effect_amount_value
+          }
+          z = z + 1
+
+        
+        end--end 12 columns loop
+      
+        y = y + 1
+      
+      end--end lines in song loop
+    
+      print("recorded lines in track in song")
+      lns_in_sng_amount = y - 1
 end
 
 --GET CURRENT LINE-----------------------------------------------------------------------------------
@@ -50,39 +97,12 @@ local function get_current_line()
     line_index = song.selected_line_index  
     cur_lin_ref = song.patterns[pattern_index].tracks[track_index]:line(line_index)
     
+    clear_columns_to_clear()--...clear our destructive columns clearing index
+    
     notes_detected = 0 --...reset the amount of detected notes to 0...
-    
-    --...we store the line values of all lines in track in song
-    local y = 1
-    for pos,line in song.pattern_iterator:lines_in_track(track_index,true) do
-    
-      lns_in_sng[y] = {}
-    
-      local z = 1
-      while z < 13 do
       
-        print("\nz = " .. z)
-      
-        lns_in_sng[y][z] = {
-        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).note_value,
-        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).instrument_value,
-        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).volume_value,
-        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).panning_value,
-        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).delay_value,
-        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).effect_number_value,
-        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).effect_amount_value
-        }
-        z = z + 1
-
-        
-      end--end 12 columns loop
-      
-      y = y + 1
-      
-    end--end lines in song loop
-    
-    print("recorded lines in track in song")
-    lns_in_sng_amount = y - 1
+    --...if the user has Non-Destructive editing activated, then store all lines in song...
+    if non_destructive then store_song_lines() end
     
     --...we store the selected line values in cur_lin_clmn_vals..
     local x = 1
@@ -95,8 +115,8 @@ local function get_current_line()
         cur_lin_ref:note_column(x).delay_value,
         cur_lin_ref:note_column(x).effect_number_value,
         cur_lin_ref:note_column(x).effect_amount_value
-    }
-    x = x + 1
+      }
+      x = x + 1
     end
     
     print("cur_lin_ref: ") print(cur_lin_ref)
@@ -112,6 +132,12 @@ local function get_current_line()
     --...and confirm the new line selection to the user in the status bar
     show_status("Line " .. line_index .. " in Pattern " .. pattern_index .. " was selected for Flourish!")
     
+    if flourish_window_created then --if we have already created a view window...
+      --...reset our sliders to 0 upon setting a new line...
+      vb.views.time_slider.value = 0
+      vb.views.tension_slider.value = 0
+    end
+    
   end--close if statement
 end
 
@@ -126,44 +152,67 @@ end
 --FLOURISH-------------------------------------------------------------------------------------------
 local function flourish()
   
-  --...we restore all the lines for the selected track in the current pattern from cur_trk_lns_obj
+  --if the user has Non-Destructive editing activated, then...
+  if non_destructive then
   
-  local y = 1
-  while y < lns_in_sng_amount do
+    --...we restore all the lines for the selected track in the current pattern from cur_trk_lns_obj  
+    local y = 1
+    while y < lns_in_sng_amount do
   
-    local z = 1
-    while z < 13 do
+      local z = 1
+      while z < 13 do
     
-      song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).note_value = lns_in_sng[y][z][1]
+        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).note_value = lns_in_sng[y][z][1]
       
-      song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).instrument_value = lns_in_sng[y][z][2]
+        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).instrument_value = lns_in_sng[y][z][2]
       
-      song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).volume_value = lns_in_sng[y][z][3]
+        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).volume_value = lns_in_sng[y][z][3]
       
-      song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).panning_value = lns_in_sng[y][z][4]
+        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).panning_value = lns_in_sng[y][z][4]
       
-      song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).delay_value = lns_in_sng[y][z][5]
+        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).delay_value = lns_in_sng[y][z][5]
       
-      song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).effect_number_value = lns_in_sng[y][z][6]
+        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).effect_number_value = lns_in_sng[y][z][6]
       
-      song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).effect_amount_value = lns_in_sng[y][z][7]
+        song.patterns[pattern_index].tracks[track_index]:line(y):note_column(z).effect_amount_value = lns_in_sng[y][z][7]
       
-      z = z + 1
+        z = z + 1
       
-    end
+      end--end while z
     
-    y = y + 1
+      y = y + 1
     
-  end
+    end--end while y
   
   print("ptn trk restored!")
   
-  for i = 1, notes_detected do
+  end--end "if non_destructive" statement
   
-    song.patterns[pattern_index].tracks[track_index]:line(line_index):note_column(i):clear()
+  --clear the line that we're flourishing
+  song.patterns[pattern_index].tracks[track_index]:line(line_index):clear()
+  
+  for i = 1, notes_detected do
 
-    --find correct line/note column to copy to
+    --find correct line to copy to
+    local line_index_to_copy_to = line_index + (math.floor(((i - 1) * time) / 256))
+    
+    --update our column clearing index if it has grown
+    if columns_to_clear[i] == nil or columns_to_clear[i] < line_index_to_copy_to then
+      columns_to_clear[i] = line_index_to_copy_to
+    end
+    
+    --find correct note column to copy to
     local column_to_copy_to = song.patterns[pattern_index].tracks[track_index]:line(line_index + (math.floor(((i - 1) * time) / 256))):note_column(i)
+    
+    if not non_destructive then
+    
+      --clear all columns in the range between the starting line and then line to copy to
+      local t = 1
+      while t < math.abs(line_index - columns_to_clear[i] - 1) do
+        song.patterns[pattern_index].tracks[track_index]:line(line_index + t):note_column(i):clear()
+        t = t + 1
+      end--end while t
+    end--end "if not non_destructive"
     
     --copy the values into the new line/note column      
     column_to_copy_to.note_value = cur_lin_clmn_vals[i][1]
@@ -212,8 +261,10 @@ function create_flourish_window()
         },
     
         vb:minislider {
-          min = -10000,
-          max = 10000,
+          id = "time_slider",
+          tooltip = "the time over which to spread the notes",
+          min = -727,
+          max = 727,
           value = 0,
           width = 20,
           height = 150,
@@ -233,6 +284,8 @@ function create_flourish_window()
         },
   
         vb:minislider {
+          id = "tension_slider",
+          tooltip = "(not yet implemented)",
           min = -1,
           max = 1,
           value = 0,
@@ -255,9 +308,32 @@ function create_flourish_window()
         text = "Auto-Apply"
       },      
       vb:checkbox {
-        value = false,
+        tooltip = "Moving the sliders will update/apply the change in realtime",
+        value = auto_apply,
         notifier = function(value)
           auto_apply = value
+        end
+      }
+    },--auto-apply checkbox row close
+    
+     vb:row {
+      margin = DEFAULT_MARGIN,
+      vb:text {
+        text = "Non-Destructive [Laggy!]"
+      },      
+      vb:checkbox {
+        tooltip = "Flourish will keep track of content and will not overwrite any pattern data until a new line is set to be edited (this stores the entire song in memory, so it is very slow, only to be used in delicate circumstances)",
+        value = non_destructive,
+        notifier = function(value)
+          --...reset our sliders to 0
+          vb.views.time_slider.value = 0
+          time = 0
+          vb.views.tension_slider.value = 0
+          tension = 0
+          flourish()
+          non_destructive = value
+          clear_columns_to_clear()--...clear our destructive columns clearing index
+          if value then store_song_lines() end
         end
       }
     },--auto-apply checkbox row close
@@ -291,7 +367,7 @@ end--end function
 
 --SHOW FLOURISH WINDOW-------------------------------------------------------------------------------
 local function show_flourish_window()
-  flourish_window_obj = app:show_custom_dialog(window_title, window_content)  
+  flourish_window_obj = app:show_custom_dialog(window_title, window_content)
 end
 
 --MAIN FUNCTION--------------------------------------------------------------------------------------
@@ -308,6 +384,11 @@ end
 
 renoise.tool():add_menu_entry {
   name = "Main Menu:Tools:M.O.Marmalade:Flourish...",
+  invoke = function() main_function() end
+}
+
+renoise.tool():add_menu_entry {
+  name = "Pattern Editor:Flourish...",
   invoke = function() main_function() end
 }
 
